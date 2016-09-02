548652ca52d5d62af37e1e1b70d9f984381ac19a
devkeydet <devkeydet>
2016-09-01 20:00:17 -0400

    Added code to initiate code Migration to Startup.cs

--- src/TrafficManagerWebAppSqlDbHaDr/Startup.cs
+++ src/TrafficManagerWebAppSqlDbHaDr/Startup.cs
@@ -48,8 +48,9 @@ namespace TrafficManagerWebAppSqlDbHaDr
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, AppDbContext dbContext)
         {
+            dbContext.Database.Migrate();
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
